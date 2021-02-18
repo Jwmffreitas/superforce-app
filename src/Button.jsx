6418f4f17f18c79e0176243d0c05bc6e329c7665
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-02-18 19:16:53 -0300

    corrigindo props do modal

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,17 +3,20 @@ import Pop from './Pop'
 
 export default class Button extends React.Component {
     render() {
+        var personagemID = this.props.personagem.id
+        console.log(personagemID)
+
         function openPop() {
             const modal = document.querySelector('.modal')  
             modal.style.display = "block"
             
-            document.querySelector('#Raditz').style.display = "block"
+            document.querySelector(`#${personagemID}`).style.display = "block"
         }
 
         return(
             <Fragment>
                 <button onClick={openPop}>Visualizar</button>
-                <Pop id={this.props.personagem.nome} personagem={this.props.personagem}/>
+                <Pop personagem={this.props.personagem}/>
             </Fragment>
         )
     }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 export default class Modal extends React.Component {
     render() {
+        var personagemID1 = this.props.char1
+        var personagemID2 = this.props.char2
+        var personagemID3 = this.props.char3
+        console.log(personagemID1, personagemID2, personagemID3)
+
         function closeModal() {
             const modal = document.querySelector('.modal')
             modal.style.display = "none"
 
-            document.querySelector('#Raditz').remove()
+            document.querySelector(`#${personagemID1}`).style.display = "none"
+            document.querySelector(`#${personagemID2}`).style.display = "none"
+            document.querySelector(`#${personagemID3}`).style.display = "none"
         }
 
         return(
