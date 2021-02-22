8d154834b8fbff691b51d58266978f07547476b8
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-02-22 14:14:37 -0300

    criando pop do personagem

--- src/Pop.jsx
+++ src/Pop.jsx
@@ -5,7 +5,16 @@ export default class Pop extends React.Component {
     render() {
         return(
             <div id={this.props.personagem.id} className="pop">
-                <h1>{this.props.personagem.nome}</h1>
+                <div style={{boxShadow: 'gray 5px 0px 5px'}}>
+                    <img src={this.props.personagem.imagem}></img>
+                </div>
+                <div>
+                    <ul>
+                        <li>Especial 1</li>
+                        <li>Especial 2</li>
+                        <li>Especial 3</li>
+                    </ul>
+                </div>
                 {/*<CardNewChar char={this.props.personagem}/>*/}
             </div>
         )
