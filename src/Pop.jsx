2adc2ca9410979df02624b37eb4f6880d3494d9d
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-02-22 18:14:27 -0300

    trocarSRC funcionando

--- src/Pop.jsx
+++ src/Pop.jsx
@@ -11,22 +11,24 @@ export default class Pop extends React.Component {
 
         function trocarSRC(x) {
             if(x==1) {
-                document.querySelector(idVideo).setAttribute('src', especial1)
+                document.querySelector(`#${idVideo}`).setAttribute('src', especial1)
             }
             else if(x==2) {
-                document.querySelector(idVideo).setAttribute('src', especial2)
+                document.querySelector(`#${idVideo}`).setAttribute('src', especial2)
+            }else {
+                document.querySelector(`#${idVideo}`).setAttribute('src', especial3)
             }
         }
         return(
             <div id={this.props.personagem.id} className="pop">
                 <div style={{boxShadow: 'gray 5px 0px 5px'}}>
-                <iframe id={idVideo} width="560" height="315" src={especial1} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe id={idVideo} width="560" height="315" src={especial1} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div>
                     <ul>
-                        <li>Especial 1</li>
-                        <li onClick={trocarSRC}>Especial 2</li>
-                        <li>Especial 3</li>
+                        <li onClick={() => trocarSRC(1)}>Especial 1</li>
+                        <li onClick={() => trocarSRC(2)}>Especial 2</li>
+                        <li onClick={() => trocarSRC(3)}>Especial 3</li>
                     </ul>
                 </div>
                 {/*<CardNewChar char={this.props.personagem}/>*/}
