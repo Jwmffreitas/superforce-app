4faeb67808c6146b52cc3ef38c028fecbafd82ed
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-02-22 15:01:35 -0300

    passando as props do pop

--- src/Pop.jsx
+++ src/Pop.jsx
@@ -3,15 +3,29 @@ import CardNewChar from './CardNewChar'
 
 export default class Pop extends React.Component {
     render() {
+        var especial1 = this.props.personagem.especiais.especial1
+        var especial2 = this.props.personagem.especiais.especial2
+        var especial3 = this.props.personagem.especiais.especial3
+
+        var idVideo = `${this.props.personagem.id}video`
+
+        function trocarSRC(x) {
+            if(x==1) {
+                document.querySelector(idVideo).setAttribute('src', especial1)
+            }
+            else if(x==2) {
+                document.querySelector(idVideo).setAttribute('src', especial2)
+            }
+        }
         return(
             <div id={this.props.personagem.id} className="pop">
                 <div style={{boxShadow: 'gray 5px 0px 5px'}}>
-                    <img src={this.props.personagem.imagem}></img>
+                <iframe id={idVideo} width="560" height="315" src={especial1} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div>
                     <ul>
                         <li>Especial 1</li>
-                        <li>Especial 2</li>
+                        <li onClick={trocarSRC}>Especial 2</li>
                         <li>Especial 3</li>
                     </ul>
                 </div>
