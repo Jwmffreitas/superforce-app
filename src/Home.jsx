d8e239de2f36b14d52e16479972b19a06cbf96ad
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-02-27 09:50:51 -0300

    link para download

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ export default class Home extends React.Component {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
                         <button>Ver Trailer</button>
-                        <button className="download">Download</button>
+                        <a href="https://mega.nz/file/6GIVXSxa#jrMv89psqydK-fokHBfP64i0dU9xI9uyKVwWHdf9NTE" style={{width: '50%'}} target="_blank"><button className="download">Download</button></a>
                     </div>
                 </div>
                 <div>
